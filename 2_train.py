814a4be141959a46f132f197c8fe2eb3c0dc2ba2
Suhani Khurana <suhani24khurana@gmail.com>
2022-01-11 07:20:18 +0000

    removing print and increasing epochs

--- 2_train.py
+++ 2_train.py
@@ -8,7 +8,7 @@ import digit_detector.train as train_
 
 DIR = '../train'
 NB_FILTERS = 32
-NB_EPOCH = 1
+NB_EPOCH = 5
 
 DETECTOR_FILE = 'detector_model.hdf5'
 RECOGNIZER_FILE = 'recognize_model.hdf5'
