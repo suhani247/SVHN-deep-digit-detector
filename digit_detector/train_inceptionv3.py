5f6e351dca45dd0d06b14ea614f2c9bf5e872e84
Suhani Khurana <suhani24khurana@gmail.com>
2022-01-25 22:54:17 -0500

    gray to rgb

--- digit_detector/train_inceptionv3.py
+++ digit_detector/train_inceptionv3.py
@@ -96,4 +96,4 @@ def train_detector(X_train, X_test, Y_train, Y_test, nb_filters = 32, batch_size
 
 def gray_to_rgb(x):
     print('converting')
-    return np.repeat(x[..., np.newaxis], 3, -1)
+    return np.repeat(x, 3, -1)
