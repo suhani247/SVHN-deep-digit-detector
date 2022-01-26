5b43482e0a4d8dccc557849f757299f6d41c3bb8
Suhani Khurana <suhani24khurana@gmail.com>
2022-01-25 21:41:51 -0500

    resize and load in batches

--- digit_detector/train_inceptionv3.py
+++ digit_detector/train_inceptionv3.py
@@ -2,20 +2,31 @@ from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.optimizers import Adam
 import tensorflow.keras.layers as layers
 import tensorflow.keras.models as models
+import tensorflow.keras as Keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator as ImageDataGenerator
 from matplotlib import pyplot as plt
 import numpy as np
 
 def train_detector(X_train, X_test, Y_train, Y_test, nb_filters = 32, batch_size=32, nb_epoch=5, nb_classes=2, do_augment=False, save_file='models/detector_model.hdf5'):
-    # input image dimensions
-    img_rows, img_cols = X_train.shape[1], X_train.shape[2]
-    input_shape = (img_rows, img_cols, 3)
 
    # X_train = X_train[:3000]
    # X_test = X_test[:2000]
     rgb_X_train = np.repeat(X_train[..., np.newaxis], 3, -1)
     rgb_X_test = np.repeat(X_test[..., np.newaxis], 3, -1)
+
+    #resize images
+    input_tensor = Keras.Input(shape=(32, 32, 3))
+    input_tensor_resize = layers.Lambda(
+        lambda image: Keras.backend.resize_images(
+            image, (int(100 / 32)), (int(100 / 32)),
+            "channels_last")
+    )(input_tensor)
+
+    # zero padding to get edges info, and a bigger picture
+    y = layers.ZeroPadding2D(padding=4)(input_tensor_resize)
+
     #load pre trained model, exlcuding the last layer
-    pre_trained_model = InceptionV3(input_shape = input_shape, include_top=False, weights='imagenet')
+    pre_trained_model = InceptionV3(include_top=False, weights='imagenet', input_tensor=y)
 
     for layer in pre_trained_model.layers:
         layer.trainable = False
@@ -28,17 +39,25 @@ def train_detector(X_train, X_test, Y_train, Y_test, nb_filters = 32, batch_size
 
     if nb_classes == 2:
         loss = 'binary_crossentropy'
-        accuracy_plt = 'accuracy_detection.pdf'
-        loss_plt = 'loss_detection.pdf'
+        accuracy_plt = 'v3_accuracy_detection.pdf'
+        loss_plt = 'v3_loss_detection.pdf'
     else:
         loss = 'categorical_crossentropy'
-        accuracy_plt = 'accuracy_recognition.pdf'
-        loss_plt = 'loss_recognition.pdf'
+        accuracy_plt = 'v3_accuracy_recognition.pdf'
+        loss_plt = 'v3_loss_recognition.pdf'
 
     model.compile(optimizer=Adam(lr=0.0001), loss=loss, metrics=['acc', 'loss'])
 
-    history = model.fit(rgb_X_train, Y_train, batch_size=batch_size, epochs=2,
-                        verbose=1, validation_data=(rgb_X_test, Y_test))
+    datagen = ImageDataGenerator(featurewise_center=True,featurewise_std_normalization=True,rotation_range=20,
+                                 width_shift_range=0.2,height_shift_range=0.2,horizontal_flip=True,validation_split=0.2)
+    datagen.fit(rgb_X_train)
+
+    history = model.fit_generator(datagen.flow(rgb_X_train, Y_train,batch_size=32, subset='training'),
+                                  verbose=1,
+                                  validation_data=
+                                  (datagen.flow(rgb_X_test, Y_test, batch_size=32, subset='validation')),
+                                  steps_per_epoch=len(rgb_X_train) / 32,
+                                  epochs=2)
     score = model.evaluate(rgb_X_test, Y_test, verbose=0)
     # summarize history for accuracy
     plt.plot(history.history['accuracy'])
@@ -48,6 +67,7 @@ def train_detector(X_train, X_test, Y_train, Y_test, nb_filters = 32, batch_size
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
     plt.savefig(accuracy_plt)
+    plt.clf()
     # summarize history for loss
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
@@ -56,6 +76,7 @@ def train_detector(X_train, X_test, Y_train, Y_test, nb_filters = 32, batch_size
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
     plt.savefig(loss_plt)
+    plt.clf()
     print('Test score:', score[0])
     print('Test accuracy:', score[1])
     model.save(save_file)
